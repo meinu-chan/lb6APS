6d3121a00bfd17f3ab828032469e7ca8a5315742
meinu-chan <62944053+meinu-chan@users.noreply.github.com>
2020-05-21 19:48:53 +0300

    help

---
+++ lbAPS6Console/ITalkable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lbAPS6Console
+{
+    interface ITalkable
+    {
+        string GetUserAnswer();
+        void ShowAsk(string ask);
+        void ShowAnswer(string answer);
+    }
+}
---
+++ lbAPS6Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lbAPS6Console
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AskMe askName = new AskMe("Hey beauty, what is your name\n My name is ","Oh, your name, so sweet. ");
+            askName.talk = new ConsoleTell();
+            askName.Ask();
+            AskMe askMyAge = new AskMe("How old are you ?\n Enter your age: ", "Fell old yet? XD");
+            askMyAge.talk = new ConsoleTell();
+            askMyAge.Ask();
+        }
+    }
+}
